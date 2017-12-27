dc9d5f6a4406ff6388d883353fb5cc7e99b580cf
akash <akashmalaviya101@outlook.com>
2017-12-26 18:52:26 -0800

    Test Class

--- Core Java/basic-app/src/com/techlabs/array/TestWelcome.java
+++ Core Java/basic-app/src/com/techlabs/array/TestWelcome.java
@@ -2,9 +2,19 @@ package com.techlabs.array;
 
 public class TestWelcome {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("This is Test class....");
-	}
+	public static void main(String[] names) {
 
+		System.out.println("This is Test class....");
+		System.out.println(names.length);
+		if (names.length>0) {
+			for (String name : names) {
+				System.out.println("Hello Mr. " + name);
+			}
+		}
+		else
+		{
+			System.out.println("Hello Mr. Akash");
+		}
+
+	}
 }
